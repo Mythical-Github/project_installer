f908324ea7fefa71f964649423967afbacd9739d
Mythical-Github <MythicalData@gmail.com>
2024-11-13 01:50:53 -0500

    Update project_updater.bat

--- assets/base/project_updater.bat
+++ assets/base/project_updater.bat
@@ -6,7 +6,7 @@ set "project_directory=%CD%"
 set "content_zip_urls=https://github.com/Mythical-Github/kf1_mods_installer/releases/latest/download/kf1_mods_installer.zip"
 set "project_updater_exe=%CD%\project_updater.exe"
 
-set "command=update_project --project_directory "%project_directory%" --content_zip_urls "%content_zip_urls%" --backup_exclusions project_updater.exe project_updater.bat LICENSE"
+set "command=update_project --project_directory "%project_directory%" --content_zip_urls "%content_zip_urls%" --backup_exclusions project_updater.exe logs project_updater.bat LICENSE"
 
 "%project_updater_exe%" %command%
 
