786efd29906a5e12a8709b8f31c4b3a9e6b7bfdf
Mythical-Github <MythicalData@gmail.com>
2024-11-14 19:41:06 -0500

    Update cli_py.py

--- src/project_updater/cli_py.py
+++ src/project_updater/cli_py.py
@@ -12,17 +12,18 @@ def cli_logic(cli_data):
 
     for command, command_info in cli_info_dict.items():
         subcommand_parser = subparsers.add_parser(command, help=f"Execute {command} command")
+
         arg_help_pairs = command_info.get('arg_help_pairs', [])
-        
-    for arg_entry in arg_help_pairs:
-        arg_name, arg_data = list(arg_entry.items())[0]
-        subcommand_parser.add_argument(
-            f"--{arg_name}",
-            type=str,
-            help=arg_data["help"],
-            required=arg_data["required"],
-            nargs="+" if arg_data["use_nargs"] == True else None
-        )
+
+        for arg_entry in arg_help_pairs:
+            for arg_name, arg_data in arg_entry.items():
+                subcommand_parser.add_argument(
+                    f"--{arg_name}",
+                    type=str,
+                    help=arg_data["help"],
+                    required=arg_data["required"],
+                    nargs="+" if arg_data["use_nargs"] else None
+                )
 
     args = parser.parse_args()
 
